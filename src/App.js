f4ee316f621bc3ffb12068f22b5a2e2af7dee4ef
leetecea <leeleticiac@gmail.com>
2022-07-25 08:24:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import Foto from './img/northern-forest.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  state = {
+    numero: 0
+  } 
+
+  add = () => {
+    if ( this.state.numero < 10 ){
+      this.setState({
+        numero: this.state.numero + 1
+      })
+    }
+  }
+
+  remove = () => {
+    if ( this.state.numero > 0 ){
+      this.setState({
+        numero: this.state.numero - 1
+      })
+    }
+  }
+
+  render(){
+    return(
+      <>
+      <main>
+      <div className='card'>
+       <h1> Contador </h1>
+       <h2>{this.state.numero}</h2>
+       <div className='rmv-add'>
+         <button className='btn remove' onClick={this.remove}> - </button>
+         <button className="btn add" onClick={this.add}> + </button>
+       </div>
+      </div>
+      </main> 
+      </>
+    )
+  }
 }
-
-export default App;
